2f12bd51cb8c63509859065d9e54e826eed3c75d
Yusif <70813650+yusiftaghiyev@users.noreply.github.com>
2023-06-07 07:20:07 +0400

    Test folder add

---
+++ test.java
@@ -0,0 +1,14 @@
+package yusif.taghiyev.cenablari;
+class yusif {
+    public static void main(String  ... yusif) {
+        System.out.println("Salam Yusif");
+        System.out.println("Necesen");
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("Sayi artir:" + i);
+
+        }
+
+        System.out.println("google.com");
+
+    }
+}
